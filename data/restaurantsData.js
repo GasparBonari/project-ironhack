6a74e6f00101ccd28c67f8203e125cc391d52920
Gaspar Bonari <gasparbonari@gmail.com>
2023-10-02 12:07:51 +0200

    Database added

---
+++ data/restaurantsData.js
@@ -0,0 +1,244 @@
+const jsonData = 
+[
+    {
+        "name": "Tasty Delights",
+        "image": "https://example.com/tasty_delights.jpg",
+        "address": "789 Oak Street, Foodtown",
+        "menu": [
+            {
+            "name": "Crispy Spring Rolls",
+            "price": 5.99,
+            "image": "https://example.com/crispy_spring_rolls.jpg"
+            },
+            {
+            "name": "Stuffed Mushrooms",
+            "price": 7.49,
+            "image": "https://example.com/stuffed_mushrooms.jpg"
+            },
+            {
+            "name": "Cheese Platter",
+            "price": 8.99,
+            "image": "https://example.com/cheese_platter.jpg"
+            },
+            {
+            "name": "Grilled Chicken Breast",
+            "price": 12.99,
+            "image": "https://example.com/grilled_chicken_breast.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Sizzling Flavors",
+        "image": "https://example.com/sizzling_flavors.jpg",
+        "address": "456 Pine Avenue, Culinary City",
+        "menu": [
+            {
+            "name": "Spicy Shrimp Tacos",
+            "price": 10.99,
+            "image": "https://example.com/spicy_shrimp_tacos.jpg"
+            },
+            {
+            "name": "Mango Salsa Chicken",
+            "price": 13.49,
+            "image": "https://example.com/mango_salsa_chicken.jpg"
+            },
+            {
+            "name": "Vegetarian Burrito Bowl",
+            "price": 9.99,
+            "image": "https://example.com/vegetarian_burrito_bowl.jpg"
+            },
+            {
+            "name": "Chocolate Lava Cake",
+            "price": 6.99,
+            "image": "https://example.com/chocolate_lava_cake.jpg"
+            }
+        ]
+    },
+
+    {
+        "name": "Savory Spoons",
+        "image": "https://example.com/savory_spoons.jpg",
+        "address": "234 Maple Lane, Flavorville",
+        "menu": [
+            {
+                "name": "Garlic Parmesan Wings",
+                "price": 8.99,
+                "image": "https://example.com/garlic_parmesan_wings.jpg"
+            },
+            {
+                "name": "Caprese Flatbread",
+                "price": 10.49,
+                "image": "https://example.com/caprese_flatbread.jpg"
+            },
+            {
+                "name": "Chocolate Hazelnut Tart",
+                "price": 7.99,
+                "image": "https://example.com/chocolate_hazelnut_tart.jpg"
+            }
+        ]
+    },
+      
+    {
+        "name": "Flaming Grillhouse",
+        "image": "https://example.com/flaming_grillhouse.jpg",
+        "address": "789 Cedar Avenue, Spice City",
+        "menu": [
+            {
+                "name": "Sizzling Sirloin Steak",
+                "price": 16.99,
+                "image": "https://example.com/sizzling_sirloin_steak.jpg"
+            },
+            {
+                "name": "BBQ Ribs Platter",
+                "price": 18.49,
+                "image": "https://example.com/bbq_ribs_platter.jpg"
+            },
+            {
+                "name": "Caramelized Pineapple Dessert",
+                "price": 8.99,
+                "image": "https://example.com/caramelized_pineapple_dessert.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Zen Noodles",
+        "image": "https://example.com/zen_noodles.jpg",
+        "address": "567 Bamboo Street, Tranquil Town",
+        "menu": [
+            {
+                "name": "Vegetable Pad Thai",
+                "price": 11.99,
+                "image": "https://example.com/vegetable_pad_thai.jpg"
+            },
+            {
+                "name": "Teriyaki Tofu Bowl",
+                "price": 12.49,
+                "image": "https://example.com/teriyaki_tofu_bowl.jpg"
+            },
+            {
+                "name": "Green Tea Matcha Ice Cream",
+                "price": 6.99,
+                "image": "https://example.com/green_tea_matcha_ice_cream.jpg"
+            }
+        ]
+    },
+
+    {
+        "name": "Sunny Bites",
+        "image": "https://example.com/sunny_bites.jpg",
+        "address": "123 Sunshine Street, Brightville",
+        "menu": [
+            {
+                "name": "Sunflower Salad",
+                "price": 9.99,
+                "image": "https://example.com/sunflower_salad.jpg"
+            },
+            {
+                "name": "Citrus Grilled Salmon",
+                "price": 14.49,
+                "image": "https://example.com/citrus_grilled_salmon.jpg"
+            },
+            {
+                "name": "Mango Tango Smoothie",
+                "price": 5.99,
+                "image": "https://example.com/mango_tango_smoothie.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Fiesta Fiesta",
+        "image": "https://example.com/fiesta_fiesta.jpg",
+        "address": "789 Carnival Street, Celebration City",
+        "menu": [
+            {
+                "name": "Nachos Supreme",
+                "price": 11.99,
+                "image": "https://example.com/nachos_supreme.jpg"
+            },
+            {
+                "name": "Enchilada Explosion",
+                "price": 13.49,
+                "image": "https://example.com/enchilada_explosion.jpg"
+            },
+            {
+                "name": "Churros Delight",
+                "price": 6.99,
+                "image": "https://example.com/churros_delight.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Mystic Eats",
+        "image": "https://example.com/mystic_eats.jpg",
+        "address": "456 Enchanted Lane, Fantasyland",
+        "menu": [
+            {
+                "name": "Dragon Fire Pasta",
+                "price": 12.99,
+                "image": "https://example.com/dragon_fire_pasta.jpg"
+            },
+            {
+                "name": "Unicorn Burger",
+                "price": 15.49,
+                "image": "https://example.com/unicorn_burger.jpg"
+            },
+            {
+                "name": "Magical Berry Tart",
+                "price": 7.99,
+                "image": "https://example.com/magical_berry_tart.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Cozy Corner Cafe",
+        "image": "https://example.com/cozy_corner_cafe.jpg",
+        "address": "234 Tranquil Terrace, Serenity Town",
+        "menu": [
+            {
+                "name": "Egg Benedict Bliss",
+                "price": 10.99,
+                "image": "https://example.com/egg_benedict_bliss.jpg"
+            },
+            {
+                "name": "Avocado Toast Delight",
+                "price": 8.49,
+                "image": "https://example.com/avocado_toast_delight.jpg"
+            },
+            {
+                "name": "Caramel Macchiato",
+                "price": 4.99,
+                "image": "https://example.com/caramel_macchiato.jpg"
+            }
+        ]
+    },
+    
+    {
+        "name": "Ocean Feast",
+        "image": "https://example.com/ocean_feast.jpg",
+        "address": "567 Seaside Drive, Coastal City",
+        "menu": [
+            {
+                "name": "Seafood Paella",
+                "price": 17.99,
+                "image": "https://example.com/seafood_paella.jpg"
+            },
+            {
+                "name": "Grilled Octopus",
+                "price": 19.49,
+                "image": "https://example.com/grilled_octopus.jpg"
+            },
+            {
+                "name": "Coconut Lime Sorbet",
+                "price": 6.99,
+                "image": "https://example.com/coconut_lime_sorbet.jpg"
+            }
+        ]
+    }
+]
+
+module.exports = jsonData;
--- db/index.js
+++ db/index.js
@@ -1,15 +1,16 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
+const saveDataToDatabase = require("../data/saveDataToMongo");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/name-of-project";
+  process.env.MONGODB_URI || "mongodb+srv://projectfood321:Ironhack321@cluster0.fmnd3pc.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 
-mongoose
-  .connect(MONGO_URI)
+  mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((x) => {
     const databaseName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
