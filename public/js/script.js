553a0eafe9a982a8a175cc2ef608e0133e96bb5e
Gaspar Bonari <89667053+GasparBonari@users.noreply.github.com>
2023-10-11 17:45:20 +0200

    checkout page done

--- public/js/script.js
+++ public/js/script.js
@@ -6,6 +6,23 @@ function addToCart(name, price) {
 
   // Update the cart list
   updateCartList();
+
+  // Send updated cart data to the server
+  updateCartOnServer(cartItems);
+}
+
+// Function to send updated cart data to the server
+function updateCartOnServer(cartItems) {
+  fetch('/updateCart', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ cartItems }),
+  })
+    .then(response => response.json())
+    .then(data => console.log('Cart updated on server:', data))
+    .catch(error => console.error('Error updating cart on server:', error));
 }
 
 function updateCartList() {
