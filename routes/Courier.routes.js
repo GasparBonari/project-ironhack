fe712a6980b5928e4daa5ca2097f6d07667fa51d
dutuka <39162585+dutuka@users.noreply.github.com>
2023-10-20 20:19:28 +0200

    organize, fine-tuning dashboard for manager and courier, remove delete button from courier order list

--- routes/Courier.routes.js
+++ routes/Courier.routes.js
@@ -1,30 +1,35 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = new Router();
-const Courier = require('../models/Courier.models'); // Import the Courier model
-const Order = require('../models/Order.model'); 
-const mongoose = require('mongoose');
-const bcryptjs = require('bcryptjs');
+const Courier = require("../models/Courier.models"); // Import the Courier model
+const Order = require("../models/Order.model");
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
 // Middleware
-const { courierLoggedin, courierLoggedout, isManagerAndLoggedIn } = require('../middleware/route-guard.js');
+const {
+  courierLoggedin,
+  courierLoggedout,
+  isManagerAndLoggedIn,
+} = require("../middleware/route-guard.js");
 
 // GET /manager/login
-router.get('/courierLogin', courierLoggedout, (req, res) => {
-  res.render('Manager/courierLogin');
+router.get("/courierLogin", courierLoggedout, (req, res) => {
+  res.render("Manager/courierLogin");
 });
 
 // POST /manager/login
-router.post('/courierLogin', (req, res, next) => {
-  console.log('here it was hit');
-  console.log('SESSION =====> ', req.session);
+router.post("/courierLogin", (req, res, next) => {
+  console.log("here it was hit");
+  console.log("SESSION =====> ", req.session);
 
   const { username, password } = req.body;
 
   // Check that username and password are provided
-  if (username === '' || password === '') {
-    return res.render('Manager/courierLogin', {
-      errorMessage: 'All fields are mandatory. Please provide username and password.',
+  if (username === "" || password === "") {
+    return res.render("Manager/courierLogin", {
+      errorMessage:
+        "All fields are mandatory. Please provide username and password.",
     });
   }
 
@@ -32,27 +37,32 @@ router.post('/courierLogin', (req, res, next) => {
   Courier.findOne({ username })
     .then((courier) => {
       if (!courier) {
-        console.log('Login failed, account not registered.');
-        return res.render('Manager/courierLogin', {
-          errorMessage: 'User not found and/or incorrect password, please try again!',
+        console.log("Login failed, account not registered.");
+        return res.render("Manager/courierLogin", {
+          errorMessage:
+            "User not found and/or incorrect password, please try again!",
         });
       } else if (bcryptjs.compareSync(password, courier.passwordHash)) {
         // Save the user in the session
         req.session.currentUser = courier;
-        console.log('Session data after login:', req.session);
-        return res.redirect('/manager/courierMain');
+        console.log("Session data after login:", req.session);
+        return res.redirect("/manager/courierMain");
       } else {
-        console.log('Incorrect password.');
-        return res.render('Manager/courierLogin', {
-          errorMessage: 'User not found and/or incorrect password, please try again!',
+        console.log("Incorrect password.");
+        return res.render("Manager/courierLogin", {
+          errorMessage:
+            "User not found and/or incorrect password, please try again!",
         });
       }
     })
     .catch((error) => {
       if (error.code === 11000) {
-        console.log('User not found and/or incorrect password, please try again!');
-        return res.status(500).render('Manager/courierLogin', {
-          errorMessage: 'User not found and/or incorrect password, please try again!',
+        console.log(
+          "User not found and/or incorrect password, please try again!"
+        );
+        return res.status(500).render("Manager/courierLogin", {
+          errorMessage:
+            "User not found and/or incorrect password, please try again!",
         });
       } else {
         next(error);
@@ -61,20 +71,25 @@ router.post('/courierLogin', (req, res, next) => {
 });
 
 // GET route to display the signup form for Courier
-router.get('/courierCreate', courierLoggedout, (req, res) => {
-  res.render('Manager/courierCreate');
+router.get("/courierCreate", courierLoggedout, (req, res) => {
+  res.render("Manager/courierCreate");
 });
 
 // POST route to process form data for Courier registration
-router.post('/courierCreate', (req, res, next) => {
-  console.log('The form data: ', req.body);
+router.post("/courierCreate", (req, res, next) => {
+  console.log("The form data: ", req.body);
 
   const { username, email, password, password2 } = req.body;
 
   if (!username || !email || !password) {
-    return res.render('Manager/courierCreate', { errorMessage: 'All fields are mandatory. Please provide your username, email, and password.' });
+    return res.render("Manager/courierCreate", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email, and password.",
+    });
   } else if (password !== password2) {
-    return res.render('Manager/courierCreate', { errorMessage: 'Passwords do not match. Please, try again.' });
+    return res.render("Manager/courierCreate", {
+      errorMessage: "Passwords do not match. Please, try again.",
+    });
   }
 
   // Make sure passwords are strong:
@@ -82,11 +97,15 @@ router.post('/courierCreate', (req, res, next) => {
   if (!regex.test(password)) {
     res
       .status(500)
-      .render('Manager/courierCreate', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+      .render("Manager/courierCreate", {
+        errorMessage:
+          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+      });
     return;
   }
 
-  bcryptjs.genSalt(saltRounds)
+  bcryptjs
+    .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
     .then((hashedPassword) => {
       return Courier.create({
@@ -96,16 +115,18 @@ router.post('/courierCreate', (req, res, next) => {
       });
     })
     .then((userFromDB) => {
-      console.log('Newly created user is: ', userFromDB);
+      console.log("Newly created user is: ", userFromDB);
       return res.redirect(`/manager/courierNew/${userFromDB.username}`);
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
-        return res.status(500).render('Manager/courierCreate', { errorMessage: error.message });
+        return res
+          .status(500)
+          .render("Manager/courierCreate", { errorMessage: error.message });
       } else if (error.code === 11000) {
-        console.log('Username or email is already in use.');
-        return res.status(500).render('Manager/courierCreate', {
-          errorMessage: 'Username or email is already in use.',
+        console.log("Username or email is already in use.");
+        return res.status(500).render("Manager/courierCreate", {
+          errorMessage: "Username or email is already in use.",
         });
       } else {
         next(error);
@@ -114,16 +135,16 @@ router.post('/courierCreate', (req, res, next) => {
 });
 
 // GET route to display Courier main page
-router.get('/courierMain', courierLoggedin, (req, res) => {
-  res.render('Manager/courierMain', {
+router.get("/courierMain", courierLoggedin, (req, res) => {
+  res.render("Manager/courierMain", {
     userInSession: req.session.currentUser,
   });
 });
 
 // GET route for courierNew
-router.get('/courierNew/:username', (req, res) => {
+router.get("/courierNew/:username", (req, res) => {
   const { username } = req.params;
-  res.render('Manager/courierNew', { username });
+  res.render("Manager/courierNew", { username });
 });
 
 // edit/delete Courier profile:
@@ -145,13 +166,7 @@ router.get("/courier/:CourierId/edit", courierLoggedin, (req, res, next) => {
 
 router.post("/courier/:CourierId/edit", courierLoggedin, (req, res, next) => {
   const { CourierId } = req.params;
-  const {
-    username,
-    email,
-    birthday,
-    country,
-    city
-  } = req.body;
+  const { username, email, birthday, country, city } = req.body;
 
   Courier.findByIdAndUpdate(
     CourierId,
@@ -160,7 +175,7 @@ router.post("/courier/:CourierId/edit", courierLoggedin, (req, res, next) => {
       email,
       birthday,
       country,
-      city
+      city,
     },
     { new: true }
   )
@@ -171,26 +186,29 @@ router.post("/courier/:CourierId/edit", courierLoggedin, (req, res, next) => {
     .catch((error) => next(error));
 });
 
-router.post("/courierMain/:CourierId/delete", courierLoggedin, (req, res, next) => {
-  const { CourierId } = req.params; 
-  Courier.findByIdAndDelete(CourierId)
-    .then(() => {
-      req.session.destroy((err) => {
-        if (err) next(err);
-        res.render("Manager/courierDeleted");
-      });
-    })
-    .catch((error) => next(error));
-});
+router.post(
+  "/courierMain/:CourierId/delete",
+  courierLoggedin,
+  (req, res, next) => {
+    const { CourierId } = req.params;
+    Courier.findByIdAndDelete(CourierId)
+      .then(() => {
+        req.session.destroy((err) => {
+          if (err) next(err);
+          res.render("Manager/courierDeleted");
+        });
+      })
+      .catch((error) => next(error));
+  }
+);
 
 // POST route to log out a Courier
-router.post('/courierLogout', courierLoggedin, (req, res, next) => {
+router.post("/courierLogout", courierLoggedin, (req, res, next) => {
   req.session.destroy((err) => {
     if (err) return next(err);
-    return res.render('Manager/courierLogin');
+    return res.render("Manager/courierLogin");
   });
 });
-
 
 // CRUD Couriers BEGIN
 
@@ -259,7 +277,8 @@ router.post(
 );
 
 router.get(
-  "/managerMain/courierList/:CourierId/edit", isManagerAndLoggedIn,
+  "/managerMain/courierList/:CourierId/edit",
+  isManagerAndLoggedIn,
   (req, res, next) => {
     const { CourierId } = req.params;
     Courier.findById(CourierId)
@@ -283,7 +302,7 @@ router.post(
   isManagerAndLoggedIn,
   (req, res, next) => {
     const { CourierId } = req.params;
-    const { username, email, birthday, country, city  } = req.body;
+    const { username, email, birthday, country, city } = req.body;
     Courier.findByIdAndUpdate(
       CourierId,
       {
@@ -303,22 +322,17 @@ router.post(
 );
 // CRUD Manager END
 
-
 //List of all Orders:
 
-router.get(
-  "/courierMain/orderList",
-  courierLoggedin,
-  (req, res, next) => {
-    Order.find()
-      .then((allOrders) => {
-        res.render("Manager/orderList", { orders: allOrders });
-      })
-      .catch((error) => {
-        console.log("Couriers error: ", error);
-        next(error);
-      });
-  }
-);
+router.get("/courierMain/orderList", courierLoggedin, (req, res, next) => {
+  Order.find()
+    .then((allOrders) => {
+      res.render("Manager/courierOrderList", { orders: allOrders });
+    })
+    .catch((error) => {
+      console.log("Couriers error: ", error);
+      next(error);
+    });
+});
 
 module.exports = router;
